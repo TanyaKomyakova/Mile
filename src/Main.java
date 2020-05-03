d5fd112adb936f6a9fe63d8d1b8ff024ab485190
TanyaKomyakova <tanya_komyakova@mail.ru>
2020-05-03 13:26:48 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 957050;
+        int bonus = 20;
+
+        int mile = ticket / bonus / 100;
+        System.out.println(mile);
+    }
+}
